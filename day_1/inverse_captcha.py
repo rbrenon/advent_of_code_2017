2915650ff999da885eb96ac35f67dcfbff6fc056
ryan brenon <rbrenon@gmail.com>
2022-11-12 09:27:23 -0500

    day 1-2

--- day_1/inverse_captcha.py
+++ day_1/inverse_captcha.py
@@ -5,9 +5,12 @@ def main():
         raw_input = f.readlines()
 
     input_list = [int(num) for num in raw_input.pop()]
+    part_one(input_list)
+    part_two(input_list)
 
+
+def part_one(input_list: list[int]) -> None:
     number_of_elements = len(input_list)
-
     captcha_sum = 0
 
     for index, digit in enumerate(input_list):
@@ -17,9 +20,20 @@ def main():
         elif digit == input_list[index+1]:
             captcha_sum += digit
 
-        print(f"index: {index}, number: {int(digit)}")
+    print(f"part one: captcha sum = {captcha_sum}") # 995
 
-    print(f"captcha sum = {captcha_sum}")
+
+def part_two(input_list: list[int]) -> None:
+    number_of_elements = len(input_list)
+    captcha_sum = 0
+
+    for index, digit in enumerate(input_list):
+        halfway_digit_index = (index + int(number_of_elements / 2)) % number_of_elements
+        halfway_digit = input_list[halfway_digit_index]
+        if digit == halfway_digit:
+            captcha_sum += digit
+
+    print(f"part twwo: captcha sum = {captcha_sum}") # low 594, 974;  high: 1302
 
 
 if __name__ == "__main__":
